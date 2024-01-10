350913768b00e0074b5ec56ad07c5665beab5781
Arthur Beck <averse.abfun@gmail.com>
2024-01-10 14:54:29 +0000

    :(

---
+++ script.js
@@ -0,0 +1,62 @@
+if (!document.location.hash) {
+  document.location.hash = document.querySelector('.show-start').id;
+  document.location.reload();
+} else {
+  document.querySelector(document.location.hash).style.display = 'block';
+  var func = function () {
+    document.querySelector(document.location.hash).style.opacity =
+      new Number(document.querySelector(document.location.hash).style.opacity) +
+      0.1;
+    if (document.querySelector(document.location.hash).style.opacity == 1) {
+      clearTimeout(timeout);
+    } else {
+      timeout = setTimeout(function () {
+        func();
+      }, 50);
+    }
+  };
+  var timeout = setTimeout(func, 50);
+  if (
+    '#' + document.querySelector('.show-start').id ==
+    document.location.hash
+  ) {
+    document.querySelector('.title').style.display = 'none';
+  }
+}
+
+function change_page_wrapper(page) {
+  return function () {
+    var func = function () {
+      document.querySelector(document.location.hash).style.opacity =
+        new Number(
+          document.querySelector(document.location.hash).style.opacity
+        ) - 0.1;
+      if (document.querySelector(document.location.hash).style.opacity == 0) {
+        clearTimeout(timeout);
+        document.querySelector(document.location.hash).style.display = 'none';
+        document.location.hash = page;
+        document.location.reload();
+      } else {
+        timeout = setTimeout(function () {
+          func();
+        }, 50);
+      }
+    };
+    var timeout = setTimeout(func, 50);
+  };
+}
+
+var buttons_change = Array.from(document.querySelectorAll('.change-page'));
+for (var i in buttons_change) {
+  var item = buttons_change[i];
+  if (!item.disabled) {
+    if (!document.getElementById(item.dataset.toPage)) {
+      console.log(
+        `DANGLING BUTTON!!!! It tries to lead to ${item.dataset.toPage}!`
+      );
+    }
+    item.addEventListener('click', change_page_wrapper(item.dataset.toPage));
+  }
+}
+var pages = Array.from(document.querySelectorAll('.page'));
+console.log(`${pages.length} pages`);
