e6dd35bf5308aa4124f10413d5bd9dcefaf1a062
Arthur Beck <averse.abfun@gmail.com>
2024-01-10 16:05:59 +0000

    yaaayay pages work

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+try {
 if (!document.location.hash) {
   document.location.hash = document.querySelector('.show-start').id;
   document.location.reload();
 } else {
-  document.querySelector(document.location.hash).style.display = 'block';
+  (document.querySelector(document.location.hash) || {style: {display: ""}}).style.display = 'block';
   var func = function () {
-    document.querySelector(document.location.hash).style.opacity =
-      new Number(document.querySelector(document.location.hash).style.opacity) +
+    (document.querySelector(document.location.hash) || {style: {opacity: 0}}).style.opacity =
+      new Number((document.querySelector(document.location.hash) || {style: {opacity: 0}}).style.opacity) +
       0.1;
-    if (document.querySelector(document.location.hash).style.opacity == 1) {
+    if ((document.querySelector(document.location.hash) || {style: {opacity: 1}}).opacity == 1) {
       clearTimeout(timeout);
     } else {
       timeout = setTimeout(function () {
@@ -16,12 +17,6 @@ if (!document.location.hash) {
     }
   };
   var timeout = setTimeout(func, 50);
-  if (
-    '#' + document.querySelector('.show-start').id ==
-    document.location.hash
-  ) {
-    document.querySelector('.title').style.display = 'none';
-  }
 }
 
 function change_page_wrapper(page) {
@@ -60,3 +55,6 @@ for (var i in buttons_change) {
 }
 var pages = Array.from(document.querySelectorAll('.page'));
 console.log(`${pages.length} pages`);
+} catch(e) {
+  document.querySelector("nav").innerHTML += e.toString();
+}
